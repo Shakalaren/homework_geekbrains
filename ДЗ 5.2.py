037deff076a493f56b867077bfab8444f6b0399e
Shak <Kostuchkovlaf@yandex.ru>
2021-09-27 22:22:24 +0300

    Внесение изменений в ДЗ 5.2*

--- ДЗ 5.2.py
+++ ДЗ 5.2.py
@@ -1,4 +1,5 @@
-numbers_gen = (num for num in range(1, 100) if num % 2 != 0)
+n = int(input("Введите число: "))
+numbers_gen = (num for num in range(1, n + 1, 2))
 
 for i in numbers_gen:
     print(i)
