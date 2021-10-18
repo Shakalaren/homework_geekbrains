a78038cc6352866dae5d23d4e5ba74ea9fd09cd5
Shak <Kostuchkovlaf@yandex.ru>
2021-10-18 18:01:25 +0300

    ДЗ 11.2

---
+++ 11.2.py
@@ -0,0 +1,22 @@
+class MyOwnErr(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+Num1 = int(input("Введите число 1: "))
+Num2 = int(input("Введите число 2: "))
+UserInput = input("Введите +, -, * или / для совершения математического действия: ")
+
+try:
+    if UserInput == "+":
+        print(f'Сумма {Num1} и {Num2} равняется {Num1 + Num2}')
+    elif UserInput == "-":
+        print(f'Разница {Num1} и {Num2} равняется {Num1 - Num2}')
+    elif UserInput == "*":
+        print(f'Произведение {Num1} и {Num2} равняется {Num1 * Num2}')
+    elif Num2 == 0 and UserInput == "/":
+        raise MyOwnErr("Делить на 0 не представляется возможным")
+    elif UserInput == "/":
+        print(f'Частное {Num1} и {Num2} равняется {Num1 / Num2}')
+except (ValueError, MyOwnErr) as er:
+    print(er)
